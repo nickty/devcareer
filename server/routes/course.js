0393f6ea7ce5e8042f5320428c58ad5fed575d4f
nickty <nickty.86@gmail.com>
2022-01-25 12:39:29 +0600

    Is instructor middleware and create endpoint

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const { uploadImage, removeImage } = require('../controllers/course')
-const { requireSignin } = require('../middlewares')
+const { requireSignin, isInstructor } = require('../middlewares')
 
 const router = express.Router()
 
 
 router.post('/course/upload-image', uploadImage)
 router.post('/course/remove-image', removeImage)
+
+//coruse
+router.post('/course', requireSignin, isInstructor, create)
 
 
 
