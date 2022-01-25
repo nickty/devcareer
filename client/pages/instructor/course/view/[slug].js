3cfdce0f15844e78730dbd835e3c9071f7b3352f
nickty <nickty.86@gmail.com>
2022-01-25 21:26:44 +0600

    single course view

--- client/pages/instructor/course/view/[slug].js
+++ client/pages/instructor/course/view/[slug].js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import { useRouter } from 'next/router'
 import InstructorRoute from "../../../../components/routes/InstructorRoute";
 import axios from 'axios';
+import { Avatar, Tooltip } from 'antd'
+import { EditOutlined, CheckOutlined } from '@ant-design/icons'
 
 
 const CourseView = () => {
@@ -19,7 +21,29 @@ const CourseView = () => {
     }
   return <InstructorRoute>
       <div className='container-fluid pt-3'>
-          {console.log(course)}
+          {course && <div className='container-fluid pt-1'>
+              <Avatar size={80} src={course.image ? course.image.Location : '/course.png'} />
+              <div className='media-body pl-2'>
+                  <div className='row'>
+                      <div className='col'>
+                          <h5 className='mt-2 text-primary'>{course.name}</h5>
+                          <p style={{marginTop: '-10px'}}>{course.lessons && course.lessons.length} Lessons</p>
+                          <p style={{marginTop: '-15px', fontSize: '10px'}}>{course.category}</p>
+                      </div>
+                  </div>
+
+                  <div className='d-flex'>
+                    <Tooltip title="Edit">
+                        <EditOutlined className='h5 text-warning mr-4 pointer' />
+                    </Tooltip>
+                    <Tooltip title="Publish">
+                        <CheckOutlined className='h5 pointer text-danger' />
+                    </Tooltip>
+              </div>
+              
+              </div>
+              
+              </div>}
       </div>
   </InstructorRoute>;
 };
