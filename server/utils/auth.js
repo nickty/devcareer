556930c3465e7e0ea839b7304cdb588786c6c264
nickty <nickty.86@gmail.com>
2022-02-08 07:45:30 +0600

    updated from desktop pc

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,21 +1,23 @@
-const bcrypt = require('bcrypt')
+/** @format */
 
-exports.hashPassword = password => {
-    return new Promise((resolve, reject) => {
-        bcrypt.genSalt(12, (err, salt) => {
-            if(err){
-                reject(err)
-            }
-            bcrypt.hash(password, salt, (err, hash) => {
-                if(err){
-                    reject(err)
-                }
-                resolve(hash)
-            })
-        })
-    })
-}
+const bcrypt = require('bcryptjs');
+
+exports.hashPassword = (password) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(12, (err, salt) => {
+      if (err) {
+        reject(err);
+      }
+      bcrypt.hash(password, salt, (err, hash) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(hash);
+      });
+    });
+  });
+};
 
 exports.comparePassword = (password, hashedPassword) => {
-    return bcrypt.compare(password, hashedPassword);
-}
+  return bcrypt.compare(password, hashedPassword);
+};
